038a34ced0b4d612ac05e9b0c26f239fac513a7e
alevellop <alevellop@gmail.com>
2013-11-14 20:03:17 +0100

    modificado application.rb para que Heroku cargue estilo.

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,8 @@ module Pfc
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'    
+    config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false #añadido, Heroku no carga estilo   
   end
 end
