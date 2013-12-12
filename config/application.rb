28dce8e30030c5130926ba60c409b5c97b18c699
alevellop <alevellop@gmail.com>
2013-12-12 19:43:31 +0100

    modificados production.rb y application.rb

--- config/application.rb
+++ config/application.rb
@@ -60,10 +60,14 @@ module Pfc
     # parameters by using an attr_accessible or attr_protected declaration.
     #config.active_record.whitelist_attributes = true #comentado para Heroku
 
+    #añadido para Heroku
+    config.assets.initialize_on_precompile = false 
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'   
+    config.assets.version = '1.0' 
+
   end
 end
